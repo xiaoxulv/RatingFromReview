e78843068d56a90514c1e2d03755f9127392674d
Xiaoxu <xiaoxul@andrew.cmu.edu>
2015-11-07 17:33:35 -0500

    generate matrix for baseline model

--- preprocess.py
+++ preprocess.py
@@ -5,57 +5,85 @@ import time
 import pandas as pd
 import re
 import json
+from collections import Counter
+from scipy.sparse import coo_matrix
+import pickle
 
 
-json_list = []
+
+
 start_time = time.time()
-with open('yelp_reviews_train.json', 'r') as f:
+file = 'yelp_reviews_dev.json'
+#file = 'small.json'
+json_list = []
+with open(file, 'r') as f:
     for line in f:
         #print line
         json_list.append(json.loads(str(line)))
         #d = pd.read_json(line)
 print len(json_list)
 df = pd.DataFrame(json_list)
-print 'time: %ss' % (time.time()-start_time)
-
-df['text'] = df['text'].map(lambda x: x.lower()) # faster than str.lower()
-#df['text'] = df["text"].str.lower()
-print 'time: %ss' % (time.time()-start_time)
-
-#df['text'] = df['text'].str.replace('[^\w\s]|\w*\d\w*',' ')
-df['text'].replace({'[^\w\s]|\w*\d\w*': ''}, regex = True, inplace = True)
-# df['text'] = df['text'].apply(raw_string)
-# df['text'] = df['text'].apply(replace)
-print 'time: %ss' % (time.time()-start_time)
-
-df['tokens'] = df['text'].str.split()
-print 'time: %ss' % (time.time()-start_time)
 
 stop_words = []
 with open('stopword.list', 'r') as f:
     for line in f:
         stop_words.append(line.strip())
 stop_words = set(stop_words)
+print 'time: %ss' % (time.time()-start_time)
 
-#df['tokens_new'] = df['tokens'].apply(lambda x: [item for item in x if item not in stop_words])
+# def generate(str, stop):
+#     text = re.sub('[^\w\s]|\w*\d\w*', '', str.lower())
+#     counts = Counter(text.split())
+#     for x in stop.intersection(counts):
+#         del counts[x]
+#     return counts
+#
+# df['tokens'] = df['text'].apply(generate, args = (stop_words, ))
 
-def remove(l,stop):
-    l = set(l)
-    for x in stop.intersection(l):
-        if x in l:
-            l.remove(x)
-    return l
-df['tokens_new'] = df['tokens'].apply(remove, args = (stop_words,))
+tokens_dict = []
+words = []
+for i in xrange(df.shape[0]):
+    text = re.sub('[^\w\s]|\w*\d\w*', '', df['text'][i].lower())
+    counts = Counter(text.split())
+    for x in stop_words.intersection(counts):
+        del counts[x]
+    tokens_dict.append(counts)
+    words.append(counts.elements())
+
+df['tokens'] = tokens_dict
+print 'time: %ss' % (time.time()-start_time)
+
+Dict = {}
+for x in words:
+    for y in x:
+        try:
+            Dict[y] += 1
+        except:
+            Dict[y] = 1
+
 
 print 'time: %ss' % (time.time()-start_time)
 
-df['tokens_new'].to_csv('tokens.txt', index = False)
+words_select = sorted(Dict, key = Dict.__getitem__, reverse = True)[:1000]
+set_words = set(words_select)
+
+for x in xrange(9):
+    print words_select[x], Dict[words_select[x]]
+
+row = []
+col = []
+data = []
+for i in xrange(df.shape[0]):
+    d = df['tokens'][i]
+    for x in d.keys():
+        if x in set_words:
+            row.append(i)
+            col.append(words_select.index(x))
+            data.append(d[x])
+
+m  = coo_matrix((data, (row, col)))
 print 'time: %ss' % (time.time()-start_time)
-# def replace(s):
-#     return re.sub('[^\w\s]|\w*\d\w*', s, ' ')
-# def raw_string(s):
-#     if isinstance(s, str):
-#         s = s.encode('string-escape')
-#     elif isinstance(s, unicode):
-#         s = s.encode('unicode-escape')
-#     return s
+
+outfile = open('m.pickle', 'w')
+pickle.dump(m, outfile)
+print 'time: %ss' % (time.time()-start_time)
