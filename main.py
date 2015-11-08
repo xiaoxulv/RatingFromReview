9812317b371a0d3ce3c7475783518eb64f45165b
Xiaoxu <xiaoxul@andrew.cmu.edu>
2015-11-08 16:29:57 -0500

    first version multi class logistic regression

---
+++ main.py
@@ -0,0 +1,11 @@
+__author__ = 'Ariel'
+
+def main():
+    #file = 'yelp_reviews_train.json'
+    #file = 'yelp_reviews_dev.json'
+    #file = 'small.json'
+
+
+    return
+if __name__ == '__main__':
+    main()
--- preprocess.py
+++ preprocess.py
@@ -10,80 +10,75 @@ from scipy.sparse import coo_matrix
 import pickle
 
 
+def process(file):
+    start_time = time.time()
+    # load data into a Dataframe
+    json_list = []
+    with open(file, 'r') as f:
+        for line in f:
+            json_list.append(json.loads(str(line)))
+    print len(json_list)
+    df = pd.DataFrame(json_list)
+    # read in stop words list
+    stop_words = []
+    with open('stopword.list', 'r') as f:
+        for line in f:
+            stop_words.append(line.strip())
+    stop_words = set(stop_words)
+    print 'time: %ss' % (time.time()-start_time)
 
+    # text preprocess and tokenize
+    tokens_dict = []
+    words = []
+    for i in xrange(df.shape[0]):
+        # remove punctuations and words with numbers
+        text = re.sub('[^\w\s]|\w*\d\w*', '', df['text'][i].lower())
+        counts = Counter(text.split())
+        for x in stop_words.intersection(counts):
+            del counts[x]
+        tokens_dict.append(counts)
+        words.append(counts.elements())
+    df['tokens'] = tokens_dict
+    print 'time: %ss' % (time.time()-start_time)
 
-start_time = time.time()
-file = 'yelp_reviews_dev.json'
-#file = 'small.json'
-json_list = []
-with open(file, 'r') as f:
-    for line in f:
-        #print line
-        json_list.append(json.loads(str(line)))
-        #d = pd.read_json(line)
-print len(json_list)
-df = pd.DataFrame(json_list)
+    # Build overall dictionary
+    Dict = {}
+    for x in words:
+        for y in x:
+            try:
+                Dict[y] += 1
+            except:
+                Dict[y] = 1
+    print 'time: %ss' % (time.time()-start_time)
 
-stop_words = []
-with open('stopword.list', 'r') as f:
-    for line in f:
-        stop_words.append(line.strip())
-stop_words = set(stop_words)
-print 'time: %ss' % (time.time()-start_time)
+    # Model selection
+    words_select = sorted(Dict, key = Dict.__getitem__, reverse = True)[:1000]
+    set_words = set(words_select)
+    # Build model matrix of x
+    row = []
+    col = []
+    data = []
+    for i in xrange(df.shape[0]):
+        d = df['tokens'][i]
+        for x in d.keys():
+            if x in set_words:
+                row.append(i)
+                col.append(words_select.index(x))
+                data.append(d[x])
+    m  = coo_matrix((data, (row, col)))
+    print 'time: %ss' % (time.time()-start_time)
 
-# def generate(str, stop):
-#     text = re.sub('[^\w\s]|\w*\d\w*', '', str.lower())
-#     counts = Counter(text.split())
-#     for x in stop.intersection(counts):
-#         del counts[x]
-#     return counts
-#
-# df['tokens'] = df['text'].apply(generate, args = (stop_words, ))
+    # write matrix x and y to file
+    outfile = open('train.pickle', 'w')
+    pickle.dump(m, outfile)
 
-tokens_dict = []
-words = []
-for i in xrange(df.shape[0]):
-    text = re.sub('[^\w\s]|\w*\d\w*', '', df['text'][i].lower())
-    counts = Counter(text.split())
-    for x in stop_words.intersection(counts):
-        del counts[x]
-    tokens_dict.append(counts)
-    words.append(counts.elements())
+    outfile = open('trainY.pickle', 'wb')
+    pickle.dump(list(df['stars']), outfile)
+    print 'time: %ss' % (time.time()-start_time)
 
-df['tokens'] = tokens_dict
-print 'time: %ss' % (time.time()-start_time)
+    # data exploration part
+    # for x in xrange(9):
+    #     print words_select[x], Dict[words_select[x]]
+    # print df.groupby(['stars']).count()
 
-Dict = {}
-for x in words:
-    for y in x:
-        try:
-            Dict[y] += 1
-        except:
-            Dict[y] = 1
-
-
-print 'time: %ss' % (time.time()-start_time)
-
-words_select = sorted(Dict, key = Dict.__getitem__, reverse = True)[:1000]
-set_words = set(words_select)
-
-for x in xrange(9):
-    print words_select[x], Dict[words_select[x]]
-
-row = []
-col = []
-data = []
-for i in xrange(df.shape[0]):
-    d = df['tokens'][i]
-    for x in d.keys():
-        if x in set_words:
-            row.append(i)
-            col.append(words_select.index(x))
-            data.append(d[x])
-
-m  = coo_matrix((data, (row, col)))
-print 'time: %ss' % (time.time()-start_time)
-
-outfile = open('m.pickle', 'w')
-pickle.dump(m, outfile)
-print 'time: %ss' % (time.time()-start_time)
+    return
